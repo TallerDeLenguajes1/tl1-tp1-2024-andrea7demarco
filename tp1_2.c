0c30265643e8637c9ca562856cf02313fbf80384
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2024-04-08 22:42:04 -0300

    punto 4

---
+++ tp1_2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int cuadrado(int numero1);
+void cuadrado2(int numero1, int *puntero_num1);
+void Invertir(int a,int b);
+
+int main(){
+   int numero1, valor,a ,b;
+   int *puntero_num1;
+   puntero_num1=&numero1;
+   printf("Ingrese un numero entero:\n");
+   scanf("%d",&numero1);
+   printf("Ingrese dos numeros para invertir:\n");
+   scanf("%d",&a);
+   
+   scanf("%d",&b);
+
+   valor=cuadrado(numero1);
+   printf("El cuadrado de ese numero es :%d\n",valor);
+   cuadrado2(numero1,puntero_num1);
+   printf("El cuadrado de ese numero es :%d\n",numero1);
+
+   printf("numeros a invertir ingresados: a:%d y b:%d\n",a,b);
+
+   return 0;
+}
+
+int cuadrado(int numero1){
+    int valor;
+    valor=(numero1*numero1);
+    printf("direccion de la variable :%d\n",&valor);
+    return valor;
+}
+
+void cuadrado2(int numero1 ,int *puntero_num1){
+    *puntero_num1=(numero1*numero1);
+    printf("direccion de la variable:%d\n",puntero_num1);
+
+
+}
