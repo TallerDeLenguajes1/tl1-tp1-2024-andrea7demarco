eb12a7a179585d546df2f2d15c8131cc55ef3176
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2024-04-08 22:35:14 -0300

    punto_e

--- tp1_1.c
+++ tp1_1.c
@@ -3,5 +3,14 @@
 int main()
 {
     printf("Hola mundo");
-    
+    int num = 10;
+    int *punt_num;
+    punt_num = &num; // inicializo el puntero
+
+    printf("Contenido del puntero: %d\n ", *punt_num);
+    printf("la direccion de memoria almacenada por el puntero:%d\n",punt_num);
+    printf("direccion de memoria de la variable %d\n", &num);
+    printf("tamanio de memoria utilizado por esa variable usando la funcion sizeof: %d", sizeof(num));
+
+
 }
